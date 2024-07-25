0b74fb4c6386d49f7a783b03eb6d37b6bae4a633
Eufrausino <gustavo.medeiros@ufv.br>
2024-07-24 21:36:56 -0300

    Subindo arquivos da patricia e atualizando gitignore

--- src/TADs/patricia.h
+++ src/TADs/patricia.h
@@ -4,40 +4,44 @@
 //Implementações árvore PATRICIA obtidas em: 
 //https://www2.dcc.ufmg.br/livros/algoritmos/cap5/codigo/c/5.16a5.21-patricia.c
 
-#define D 8   /* depende de TipoChave */
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/time.h>
+#define D 25 /* depende de TipoChave */
 
-typedef unsigned char TipoChave; /* a definir, dependendo da aplicacao */
-typedef unsigned char TipoIndexAmp;
+//!!!!ATENÇÃO: OLHAR O TAMANHO DA MAIOR PALAVRA PARA DEFINIR D!!!!!!
+
+typedef unsigned char TipoChave[D] ; /* a definir, dependendo da aplicacao */
+typedef int TipoIndexAmp;
 typedef unsigned char TipoDib;
-typedef enum
-{
-  Interno, Externo
-} 
-TipoNo;
-typedef struct TipoPatNo* TipoArvore; 
-typedef struct TipoPatNo 
-{
-  TipoNo nt;
-  union 
-  {
-    struct 
-    {
-      TipoIndexAmp Index;
-      TipoArvore Esq, Dir;
-    } 
-    NInterno ;
-    TipoChave Chave;
-  } 
-  NO;
-} 
-TipoPatNo;
+
+typedef enum{
+    Interno,
+    Externo
+} TipoNo;
+
+typedef struct TipoPatNo *TipoArvore;
+
+typedef struct TipoPatNo{
+    TipoNo nt;
+    union{
+        struct{
+            TipoIndexAmp Index;
+            char letra;
+            TipoArvore Esq, Dir;
+        } NInterno;
+        TipoChave Chave;
+    } NO;
+} TipoPatNo;
 
 TipoDib Bit(TipoIndexAmp i, TipoChave k);
 short EExterno(TipoArvore p);
-TipoArvore CriaNoInt(int i, TipoArvore *Esq,  TipoArvore *Dir);
+TipoArvore CriaNoInt(int i, TipoArvore *Esq,  TipoArvore *Dir, char letraDiferente);
 TipoArvore CriaNoExt(TipoChave k);
 void Pesquisa(TipoChave k, TipoArvore t);
-TipoArvore InsereEntre(TipoChave k, TipoArvore *t, int i);
+TipoArvore InsereEntre(TipoChave k, TipoArvore *t, int i, char letraDiferente);
 TipoArvore Insere(TipoChave k, TipoArvore *t);
+void ImprimeArvore(TipoArvore t);
 
 #endif
--- src/main.c
+++ src/main.c
@@ -1,32 +1,61 @@
-#include <stdio.h>
-#include<stdlib.h> 
-#include <sys/time.h>
 #include "patricia.h"
+int main(int argc, char *argv[]){
+	TipoArvore a = NULL;
 
-int main()
-{
-  int opcao;
-  printf("Entre com uma das seguintas opções:\n");
-  printf("1)receber o arquivo de entrada com os textos a serem indexados;\n");
-  printf("2)construir os índices invertidos, a partir dos textos de entrada, usando os TADs PATRICIA e HASH;\n");
-  printf("3)mprimir os índices invertidos, contendo as palavras em ordem alfabética, uma por linha, com suas respectivas listas de ocorrências;\n");
-  printf("4)realizar buscas por um ou mais termo(s) de busca, nos índices construídos, individualmente, apresentando os arquivos ordenados por relevância, também individualmente para cada TAD.\n");
+	//Insere cada chave na arvore
 
-  scanf("%d", &opcao);
+	TipoChave string1 = "casa de vo";
+	a = Insere(string1, &a);
+	TipoChave string2 = "casamento";
+	a = Insere(string2, &a);
+	TipoChave string3 = "casado";
+	a = Insere(string3, &a);
+	TipoChave string4 = "amado batista";
+	a = Insere(string4, &a);
 
-  switch(opcao)
-  {
-    case(1):
-      break;
-    case(2):
-      break;
-    case(3):
-      break;
-    case(4):
-      break;
-    default:
-      break;
-  }
+	TipoChave string5 = "boneca de pano";
+	a = Insere(string5, &a);
+	TipoChave string6 = "bonito";
+	a = Insere(string6, &a);
+	TipoChave string7 = "biscoito de chocolate";
+	a = Insere(string7, &a);
+	TipoChave string8 = "amor";
+	a = Insere(string8, &a);
 
-  return 0;
-} 
+	TipoChave string9 = "duda tinder";
+	a = Insere(string9, &a);
+	TipoChave string10 = "lua de cristal";
+	a = Insere(string10, &a);
+	TipoChave string11 = "zumbido";
+	a = Insere(string11, &a);
+	TipoChave string12 = "xuxa";
+	a = Insere(string12, &a);
+
+	Pesquisa(string2, a);
+
+	ImprimeArvore(a);
+
+	int opcao;
+	printf("Entre com uma das seguintas opções:\n");
+	printf("1)receber o arquivo de entrada com os textos a serem indexados;\n");
+	printf("2)construir os índices invertidos, a partir dos textos de entrada, usando os TADs PATRICIA e HASH;\n");
+	printf("3)mprimir os índices invertidos, contendo as palavras em ordem alfabética, uma por linha, com suas respectivas listas de ocorrências;\n");
+	printf("4)realizar buscas por um ou mais termo(s) de busca, nos índices construídos, individualmente, apresentando os arquivos ordenados por relevância, também individualmente para cada TAD.\n");
+
+	scanf("%d", &opcao);
+
+	switch(opcao)
+	{
+		case(1):
+		break;
+		case(2):
+		break;
+		case(3):
+		break;
+		case(4):
+		break;
+		default:
+		break;
+	}
+	return 0;
+}
